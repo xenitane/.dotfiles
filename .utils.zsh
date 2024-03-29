71764d370f2b025e2d27ef75d0035ab7b26f67fe
xenitane <tushar01.tjdsk@gmail.com>
2024-03-30 02:11:04 +0530

    feat: spicetify

--- .utils.zsh
+++ .utils.zsh
@@ -1,11 +1,10 @@
-exec xstart
-
 export EDITOR="nvim"
 
 export CHROME_EXECUTABLE="chromium"
 
 # fnm
 export PATH="/home/xenitane/.local/share/fnm:$PATH"
-eval  $(fnm env)
+eval $(fnm env)
 
-eval $(thefuck --alias wtf)
+eval $(thefuck -a wtf)
+. "$HOME/.cargo/env"
