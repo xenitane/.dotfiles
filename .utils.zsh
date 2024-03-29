5abf870861510961da21c25dbadb0a9b86b6ba8f
xenitane <tushar01.tjdsk@gmail.com>
2024-03-30 03:26:09 +0530

    gopath set

--- .utils.zsh
+++ .utils.zsh
@@ -2,6 +2,11 @@ export EDITOR="nvim"
 
 export CHROME_EXECUTABLE="chromium"
 
+# go
+export GOPATH="$(go env GOPATH)"
+export PATH="$GOPATH/bin:$PATH"
+export GOPATH="$GOPATH:$HOME/code/projects/go"
+
 # fnm
 export PATH="/home/xenitane/.local/share/fnm:$PATH"
 eval $(fnm env)
